4259d1d675ce7acd16a9ca69f98f39edddd5949f
Lenny Reis <142733648+ReisLcb@users.noreply.github.com>
2025-05-12 17:38:33 +0000

    Update home.page and biblioteca-service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,25 +9,21 @@ import { BibliotecaServiceService, Livro } from '../service/biblioteca-service.s
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   imports: [IonicModule, FormsModule, NgFor, NgIf],
-  
 })
 
 export class HomePage {
-  private bibbliotecaService  = inject(BibliotecaServiceService) // Injeta as funcionalidades do serviço
-  livros:Livro[] = [] // Define um vetor vazio que conterá os livros obtidos em cada requisição
-  titulo:string = "" // Título digitado no ion-input pelo usuário
+  private bibbliotecaService  = inject(BibliotecaServiceService)
 
-  async getLivro(titulo:string|null){
-    this.bibbliotecaService.getLivros(this.titulo).subscribe({ // retorna um observable e chama o método .subscribe() para obter um valor
-      next: data =>{ // data é o valor que toda requisição retorna, é o valor que contém as informações da API que procuramos
-        data.docs.forEach((item:any) =>{ // a propriedade 'docs' contém o vetor dos livros
-          if(item.title.toLowerCase() == titulo?.toLowerCase()) this.livros.push({titulo: item.title, autor:item.author_name[0], ano: item.first_publish_year, cover_id: item.cover_i})
-            // Adiciona o livro ao vetor caso encontrar um título igual ao dado pelo usuário
-        })
-      },
+  livros:Livro[] = []
+  titulo:string = ""
+
+  async getLivro(titulo:string){
+
+    this.bibbliotecaService.getLivros(titulo).subscribe({
+      next: data => this.livros = data.docs,
       error: err => console.log("Este livro não foi encontrado", err)
     })
+
     this.titulo = ""
-    console.log(this.livros) // Mostra-nos os livros que estão no vetor ao final da requisição
   }
 }
--- src/app/service/biblioteca-service.service.ts
+++ src/app/service/biblioteca-service.service.ts
@@ -2,11 +2,11 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface Livro{ // Padroniza os livros que serão armazenar os livros no vetor
-  titulo:string
-  autor:string[]
-  ano:number
-  cover_id:string
+export interface Livro{
+  title:string
+  author_name:string[]
+  first_publish_year:number
+  cover_i:string
 }
 
 @Injectable({
@@ -15,12 +15,12 @@ export interface Livro{ // Padroniza os livros que serão armazenar os livros no
 
 export class BibliotecaServiceService {
 
-  private http = inject(HttpClient) // Injeta as funcionalidades do HttpClient
+  private http = inject(HttpClient)
+  livros:Livro[] = []
 
   constructor() { }
 
     getLivros(titulo:string):Observable<any>{
       return this.http.get<any>(`https://openlibrary.org/search.json?q=${encodeURIComponent(titulo)}`)
-      // Método get para requisitar a API
   }
 }
